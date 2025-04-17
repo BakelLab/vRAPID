64a06c3c4473b9782ce78b1cf0872342043190ab
ZainKhmk <khalil.z@northeastern.edu>
2025-04-17 13:16:14 -0400

    fix shovill script to make sure temp file is created per sample

--- workflow/scripts/run_shovill.py
+++ workflow/scripts/run_shovill.py
@@ -2,20 +2,20 @@
 
 import subprocess
 import sys
-
-import subprocess
+import os
 
 # Define file paths
 fasta_file = snakemake.input.fasta
 reads_1 = snakemake.input.reads_1
 reads_2 = snakemake.input.reads_2
 output_dir = snakemake.output.folder
+directory = os.path.dirname(fasta_file)
 
 # Step 1: Extract sequence lengths using seqkit
-subprocess.run(f"./seqkit fx2tab --length --name {fasta_file} > chromosome_lengths.txt", shell=True, check=True)
+subprocess.run(f"./seqkit fx2tab --length --name {fasta_file} > {directory}/chromosome_lengths.txt", shell=True, check=True)
 
 # Step 2: Read the chromosome lengths and run shovill for each chromosome
-with open("chromosome_lengths.txt", "r") as file:
+with open(f"{directory}/chromosome_lengths.txt", "r") as file:
     for line in file:
         chr, len = line.split()  # Split the line into chromosome and length
         chr = chr.split("|")[1]
@@ -38,5 +38,4 @@ with open("chromosome_lengths.txt", "r") as file:
             raise e
 
 # Clean up
-subprocess.run("rm chromosome_lengths.txt", shell=True, check=True)
-
+subprocess.run(f"rm {directory}/chromosome_lengths.txt", shell=True, check=True)
